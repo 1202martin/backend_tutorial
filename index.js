25836bba1e4c982921008c86422983e051ef8f3b
1202martin <martin941079@gmail.com>
2022-11-10 13:23:08 +0900

    Started off with some basic tutorials; looked at how variables and constants are used in JS

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log("Hello World");
+
+
+//Variables in JS
+//used to use "var" before ES6, but ES6 && onward we use let
+let name = 'Martin';
+console.log(name);
+
+//Constants in JS
+const interest_rate = 0.3;
+//interest_rate = 1;
+//Using the line above triggers an error in the browser because
+//you're trying to change the value of a constant, which is against the rules.
+console.log(interest_rate);
+
+
+//Primitive Types a.k.a Value Types
+//String
+//Number (basically int, float, double, etc...)
+//Boolean
+//undefined
+//null
