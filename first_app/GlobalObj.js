7466b2d76044713fc32bc767c8dd77a083208bd9
1202martin <martin941079@gmail.com>
2022-12-03 20:33:51 +0900

    added && got started on Node tutorials

---
+++ first_app/GlobalObj.js
@@ -0,0 +1,9 @@
+//Global Obj - As a part of the global scope, these objects can be called from any part of Node
+console.log('');
+setTimeout();
+clearTimeout();
+
+setInterval();
+clearInterval();
+//all of the above are examples of global objs in Node.
+//They are put under the "global" object; i.e. global.console.log(), global.setTimeout(), etc...
